69b3f72cf832bfabc6a3f042dc2904762ab2ef97
Martin Dufour <sonic99@live.ca>
2025-09-23 19:40:27 -0400

    Added sdl window for testing

--- core/main.cpp
+++ core/main.cpp
@@ -1,5 +1,14 @@
 #include "main.h"
+#include "SDL3/SDL_video.h"
+#include <SDL3/SDL.h>
 
 int main(int argc, char* argv[]) {
 	std::cout << "hello world\n";
+
+	SDL_Window* w = SDL_CreateWindow("Win", 1280, 720, SDL_WINDOW_RESIZABLE);
+
+	while (true) {
+	}
+
+	return 0;
 }
