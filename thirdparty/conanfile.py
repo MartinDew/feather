887092959e73925839141fccb3dd33faebcf0ea7
md <martin.dufour@havenstudios.com>
2025-10-10 16:11:16 -0400

    Issues trying to fetch the right generators

--- thirdparty/conanfile.py
+++ thirdparty/conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 from conan.tools.cmake import CMakeToolchain
+import os
 
 class FeatherRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -11,14 +12,21 @@ class FeatherRecipe(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("cmake/4.1.0")
+        self.tool_requires("ninja/1.13.0")
 
     def layout(self):
         # self.folders.source = ".."
         # self.folders.build = "conan/build"
         # self.folders.generators = "conan/build"
-        cmake_layout(self)
-        
+        cmake_layout(self, src_folder="..")
+        # self.folders.root = ".."
+        # self.folders.source = "."
+        # # self.folders.build = "./conan"
+        # self.folders.generators = os.path.join("build", str(self.settings.build_type), "generators")
+        # self.folders.build = os.path.join("build", str(self.settings.build_type))
+
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.user_presets_path = '../ConanPresets.json'
+        tc.user_presets_path = 'ConanPresets.json'
+        tc.absolute_paths = True
         tc.generate()
--- thirdparty/update_conan.bat
+++ thirdparty/update_conan.bat
@@ -1,2 +1,4 @@
 conan install . --build=missing --output-folder=conan
 conan install . --build=missing -s build_type=Debug --output-folder=conan
+@REM conan install . --build=missing
+@REM conan install . --build=missing -s build_type=Debug
