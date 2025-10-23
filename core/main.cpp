48b753152c72d8ae5c0e36d5b0f0f51f8ede3926
md <martin.dufour@havenstudios.com>
2025-10-23 14:20:10 -0400

    Some refactoring to properly do the config setup and add args library for args parsing

--- core/main.cpp
+++ core/main.cpp
@@ -6,11 +6,7 @@
 #include <thread>
 
 int main(int argc, char* argv[]) {
-	std::cout << "hello world\n";
-
 	SDL_Window* w = SDL_CreateWindow("Win", 1280, 720, SDL_WINDOW_RESIZABLE);
-
-	std::this_thread::sleep_for(std::chrono::seconds(5));
 
 	return 0;
 }
