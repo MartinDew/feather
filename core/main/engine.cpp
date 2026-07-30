d32331b733bf3b3b6f2b76e7e1d05facdd015d76
mrtndew <sonic99@live.ca>
2026-07-30 18:29:30 -0400

    Refactor Engine::run to include demo scene setup in beta mode

--- core/main/engine.cpp
+++ core/main/engine.cpp
@@ -27,6 +27,79 @@ Engine::~Engine() {
 	// unregister_modules();
 }
 
+#if BETA
+inline void _setup_demo_scene(WorldSim& _world_sim) {
+	// test script
+	auto w = *_world_sim.get_world();
+	Transform t1 { { 0, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
+	Transform t2 { { -2, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
+	Transform t3 { { 2, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
+	Transform t4 { { 0, 2, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
+
+	auto material = std::make_shared<PBRMaterial>();
+	material->set_base_color_factor({ .7f, .7f, .0f });
+
+	struct Move {};
+	auto s = _world_sim.create_scene("Ni");
+	_world_sim.set_active_scene(s);
+
+	auto _ = _world_sim.create_entity(s, "Box1")
+					 .emplace<Transform>(t1)
+					 .emplace<MeshInstance>(std::make_shared<BoxMesh>())
+					 .emplace<MaterialInstance>(material)
+					 .add<Move>();
+
+	_ = _world_sim.create_entity(s, "Box2")
+				.emplace<Transform>(t2)
+				.emplace<MeshInstance>(std::make_shared<BoxMesh>())
+				.emplace<MaterialInstance>(material)
+				.add<Move>();
+
+	w.entity(s, "Box3")
+			.emplace<Transform>(t3)
+			.emplace<MeshInstance>(std::make_shared<BoxMesh>())
+			.emplace<MaterialInstance>(material)
+			.add<Move>();
+
+	w.entity("BoxChild").emplace<Transform>(t4).emplace<MeshInstance>(std::make_shared<BoxMesh>()).child_of(_);
+
+	_world_sim.create_entity(s, "Floor")
+			.emplace<Transform>(
+					Vector3 { 0, -2, 0 },
+					Quaternion::create_from_yaw_pitch_roll({ 0, 0, 0 }),
+					Vector3 { 200, 0.1f, 200 }
+			)
+			.emplace<MeshInstance>(std::make_shared<BoxMesh>());
+
+	auto dir = Vector3 { -0.5f, -1.0f, -1.f };
+	Light l { .type = LightType::Directional,
+			  .position = Vector3::zero,
+			  .direction = dir,
+			  .color = Color(1.0f, 1.0f, 1.0f, 1.0f),
+			  .intensity = 10.0f,
+			  .cast_shadows = true };
+	w.entity(s, "Directional").emplace<Light>(std::move(l));
+
+	w.system<const MeshInstance, Transform>("Spin")
+			.with<Move>()
+			.kind(flecs::OnUpdate)
+			.write<Transform>()
+			.each([](flecs::iter& it, size_t, const MeshInstance& mi, Transform& t) {
+				t.rotation = t.rotation *
+						Quaternion::create_from_yaw_pitch_roll(Vector3 { 0, static_cast<real_t>(it.delta_time()), 0 });
+			});
+
+	auto q = w.query_builder<Transform, MeshInstance, MaterialInstance*>("Test")
+					 .with<ActiveScene>()
+					 .optional()
+					 .parent()
+					 .cascade()
+					 .build();
+
+	q.each([](Entity e, Transform& t, MeshInstance mi, MaterialInstance* mat) { std::cout << e.name() << std::endl; });
+}
+#endif
+
 bool Engine::run() {
 	auto current_time = start_time;
 
@@ -43,77 +116,11 @@ bool Engine::run() {
 
 	_world_sim.init();
 
-	// test script
-	{
-		auto w = *_world_sim.get_world();
-		Transform t1 { { 0, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
-		Transform t2 { { -2, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
-		Transform t3 { { 2, -1, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
-		Transform t4 { { 0, 2, -3 }, Quaternion::create_from_yaw_pitch_roll(1.f, 0, 0), Vector3::one };
-
-		auto material = std::make_shared<PBRMaterial>();
-		material->set_base_color_factor({ .7f, .7f, .0f });
-
-		struct Move {};
-		auto s = _world_sim.create_scene("Ni");
-		_world_sim.set_active_scene(s);
-
-		auto _ = _world_sim.create_entity(s, "Box1")
-						 .emplace<Transform>(t1)
-						 .emplace<MeshInstance>(std::make_shared<BoxMesh>())
-						 .emplace<MaterialInstance>(material)
-						 .add<Move>();
-
-		_ = _world_sim.create_entity(s, "Box2")
-					.emplace<Transform>(t2)
-					.emplace<MeshInstance>(std::make_shared<BoxMesh>())
-					.emplace<MaterialInstance>(material)
-					.add<Move>();
-
-		w.entity(s, "Box3")
-				.emplace<Transform>(t3)
-				.emplace<MeshInstance>(std::make_shared<BoxMesh>())
-				.emplace<MaterialInstance>(material)
-				.add<Move>();
-
-		w.entity("BoxChild").emplace<Transform>(t4).emplace<MeshInstance>(std::make_shared<BoxMesh>()).child_of(_);
-
-		_world_sim.create_entity(s, "Floor")
-				.emplace<Transform>(Vector3 { 0, -2, 0 },
-									Quaternion::create_from_yaw_pitch_roll({ 0, 0, 0 }),
-									Vector3 { 200, 0.1f, 200 })
-				.emplace<MeshInstance>(std::make_shared<BoxMesh>());
-
-		auto dir = Vector3 { -0.5f, -1.0f, -1.f };
-		Light l { .type = LightType::Directional,
-				  .position = Vector3::zero,
-				  .direction = dir,
-				  .color = Color(1.0f, 1.0f, 1.0f, 1.0f),
-				  .intensity = 10.0f,
-				  .cast_shadows = true };
-		w.entity(s, "Directional").emplace<Light>(std::move(l));
-
-		w.system<const MeshInstance, Transform>("Spin")
-				.with<Move>()
-				.kind(flecs::OnUpdate)
-				.write<Transform>()
-				.each([](flecs::iter& it, size_t, const MeshInstance& mi, Transform& t) {
-					t.rotation = t.rotation *
-							Quaternion::create_from_yaw_pitch_roll(
-										 Vector3 { 0, static_cast<real_t>(it.delta_time()), 0 });
-				});
-
-		auto q = w.query_builder<Transform, MeshInstance, MaterialInstance*>("Test")
-						 .with<ActiveScene>()
-						 .optional()
-						 .parent()
-						 .cascade()
-						 .build();
-
-		q.each([](Entity e, Transform& t, MeshInstance mi, MaterialInstance* mat) {
-			std::cout << e.name() << std::endl;
-		});
+#if BETA
+	if (LaunchSettings::get().demo_mode.Get()) {
+		_setup_demo_scene(_world_sim);
 	}
+#endif
 
 	// update
 	double accumulator = 0.0;
